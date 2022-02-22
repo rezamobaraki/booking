5ea88f75b30908f9aaea6f6ac2f1ecec39ca9d64
Mr.Rezoo <rezam578@gmail.com>
2022-02-22 23:29:09 +0330

    config urls.py and settings.py

--- app/settings.py
+++ app/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import path
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -24,7 +26,8 @@ SECRET_KEY = 'django-insecure-&n7shm!0su^0fw4e@i_2#r+0t@2wuhcmi+&lug(nf$gg1037qh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '127.0.0.1', '141.11.246.38']
+INTERNAL_IPS = ['127.0.0.1', '141.11.246.38']
 
 # Application definition
 
@@ -52,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # third party middleware
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
@@ -81,8 +83,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "booking",
+        'USER': 'postgres',
+        'PASSWORD': "postgres",
+        'HOST': 'localhost',
+        # 'HOST': '141.11.246.38',
+        'PORT': '5432',
     }
 }
 
@@ -121,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = r'/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
