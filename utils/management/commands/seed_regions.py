2048a4ef70691b1eef1ef1e9d8cc95b68f3b3dfa
MrRezoo <rezam578@gmail.com>
2022-02-25 22:17:36 +0330

    Chore(): fix seeding

--- utils/management/commands/seed_regions.py
+++ utils/management/commands/seed_regions.py
@@ -8,7 +8,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write("Seeding regions started...")
+        self.stdout.write("countries...")
         seed_countries()
+        self.stdout.write("states...")
         seed_states()
+        self.stdout.write("cities...")
         seed_cities()
         self.stdout.write("Done!")
