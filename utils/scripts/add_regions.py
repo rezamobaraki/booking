a3573c332f2bae873db220a2624e804bb62835ac
MrRezoo <rezam578@gmail.com>
2022-02-25 21:54:41 +0330

    Chore(): add regions, migration

--- utils/scripts/add_regions.py
+++ utils/scripts/add_regions.py
@@ -1,24 +1,26 @@
 import json
 
+from django.conf import settings
+
 from core.models import City, State, Country
 
 
 def seed_countries():
-    with open("../data/json/country-state-city/countries.json", encoding="utf-8") as data_file:
+    with open(f"{settings.BASE_DIR}/utils/data/json/country-state-city/countries.json", encoding="utf-8") as data_file:
         json_data = json.loads(data_file.read())
         for obj in json_data:
             Country.objects.create(**obj)
 
 
 def seed_states():
-    with open("../data/json/country-state-city/states.json", encoding="utf-8") as data_file:
+    with open(f"{settings.BASE_DIR}/utils/data/json/country-state-city/states.json", encoding="utf-8") as data_file:
         json_data = json.loads(data_file.read())
         for obj in json_data:
             State.objects.create(**obj)
 
 
 def seed_cities():
-    with open("../data/json/country-state-city/cities.json", encoding="utf-8") as data_file:
+    with open(f"{settings.BASE_DIR}/utils/data/json/country-state-city/cities.json", encoding="utf-8") as data_file:
         json_data = json.loads(data_file.read())
         for obj in json_data:
             City.objects.create(**obj)
